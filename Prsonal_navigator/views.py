a1adf5300741461ad38c31acc5ade4f5133160d6
Rohini-Hirekhan <rohinihirekhan2020@gmail.com>
2020-09-02 10:25:55 +0530

    first commit

---
+++ Prsonal_navigator/views.py
@@ -0,0 +1,14 @@
+
+# Views.py 
+
+# I have created this file 
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def ex1(request):
+    sites = ['''<h1>For Entertainment </h1><a href = "https://www.youtube.com" >youtube video</a>''',
+             '''<h1>For Interaction </h1><a href = "https://www.facebook.com" >Facebook</a>''',
+             '''<h1>For Insight   </h1><a href = "https://www.ted.com/talks" >Ted Talk</a>''',
+             '''<h1>For Internship   </h1><a href="https://internshala.com" >Internship</a>''',
+             ]
+    return HttpResponse((sites))
